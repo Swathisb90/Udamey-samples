22f775a5847b8b37114d4fba4b8569c284ed496c
Swathisb90 <83953715+Swathisb90@users.noreply.github.com>
2024-05-27 09:53:32 +0530

    Add files via upload

---
+++ Selectloop.java
@@ -0,0 +1,40 @@
+//
+//public class Selectloop {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		driver.findElement(By.id("divpaxinfo")).click();
+//
+//		   Thread.sleep(2000L);
+//
+//		   /*int i=1;
+//
+//		while(i<5)
+//
+//		{
+//
+//		driver.findElement(By.id("hrefIncAdt")).click();
+//
+//		i++;
+//
+//		}*/
+//
+//		   System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+//
+//		for(int i=1;i<5;i++)
+//
+//		{
+//
+//		driver.findElement(By.id("hrefIncAdt")).click();
+//
+//		}
+//
+//		driver.findElement(By.id("btnclosepaxoption")).click();
+//
+//		Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
+//
+//		System.out.println(driver.findElement(By.id("divpaxinfo")).getText(
+//
+//	}
+//
+//}
---
+++ shoppingexample.java
@@ -0,0 +1,109 @@
+import java.util.Arrays;
+
+import java.util.List;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class shoppingexample {
+
+	public static void main(String[] args) throws InterruptedException {
+
+// TODO Auto-generated method stub
+
+//		System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
+//
+//		WebDriver driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		ChromeDriver driver = new ChromeDriver(options);
+
+		String[] itemsNeeded = { "Cucumber", "Brocolli", "Beetroot" };
+
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
+
+		//putting implicit wait
+		
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+		addItems(driver, itemsNeeded);
+		
+		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+		
+		driver.findElement(By.xpath("//button[contains(text(),'PROCEED TO CHECKOUT')]")).click();
+		
+		driver.findElement(By.cssSelector("input.promoCode")).sendKeys("RahulShettyAcademy");
+		
+		driver.findElement(By.cssSelector("button.promoBtn")).click();
+		System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
+		
+		
+		
+		
+
+	}
+
+	public static void addItems(WebDriver driver, String[] itemsNeeded)
+
+	{
+
+		int j = 0;
+
+		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+
+		for (int i = 0; i < products.size(); i++)
+
+		{
+
+//Brocolli - 1 Kg
+
+//Brocolli,    1 kg
+
+			String[] name = products.get(i).getText().split("-");
+
+			String formattedName = name[0].trim();
+
+//format it to get actual vegetable name
+
+//convert array into array list for easy search
+
+//  check whether name you extracted is present in arrayList or not-
+
+			List itemsNeededList = Arrays.asList(itemsNeeded);
+
+			if (itemsNeededList.contains(formattedName))
+
+			{
+
+				j++;
+
+//click on Add to cart
+
+				driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+
+				if (j == itemsNeeded.length)
+
+				{
+
+					break;
+
+				}
+
+			}
+
+		}
+
+	}
+
+}
